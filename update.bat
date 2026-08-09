c98e90be03ad2b82ae7e855e5706262efc45072e
Konoshimaru <you@example.com>
2026-08-09 19:40:05 +0100

    test

--- update.bat
+++ update.bat
@@ -4,17 +4,11 @@ echo    ENVIANDO ATUALIZACOES PARA O GITHUB
 echo =========================================
 echo.
 
-:: 1. Pergunta o que foi alterado para colocar no commit
 set /p mensagem="Digite o que voce mudou neste update: "
 
-:: 2. Prepara os arquivos
 git add .
-
-:: 3. Cria o ponto de salvamento com a mensagem que voce digitou
 git commit -m "%mensagem%"
-
-:: 4. Envia para o GitHub
-git push
+git push -u origin main
 
 echo.
 echo =========================================
